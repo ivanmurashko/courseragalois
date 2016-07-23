1ad5bc762e248dffaa63f027804ee0eb201aeceb
Ivan Murashko <ivan.murashko@gmail.com>
2016-07-23 09:00:01 +0300

    lecture3: structure of finite fields important comment

--- assignments/assignment1.cpp
+++ assignments/assignment1.cpp
@@ -1,42 +1,8 @@
 // gcc -g -O0 -o assignment1 assignment1.cpp -lstdc++ 
 #include <iostream>
 
-long pow(int x, int y)
-{
-  long res = 1;
-  for (int i = 0; i < y; ++i)
-    res *= x;
-  return res;  
-}
-
-long poly(int x)
-{
-  return pow(x, 4) + pow(x, 3) + 1;  
-}
-
-
-bool test(int n)
-{
-  for (int x = 0; x < n; ++x)
-  {
-    if (poly(x) % n == 0)
-    {
-      std::cout << "N: " << n << "; X: " << x << std::endl;
-      return true;      
-    }    
-  }
-  return false;  
-}
-
 
 int main()
-{
-  for (int i = 1; i<12; ++i)
-  {
-    int n = pow(2, i);
-    bool res = test(n);    
-    std::cout << "N: " << n << " => " << res << std::endl;
-  }
-  
+{  
   return 0;  
 }
